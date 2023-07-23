371341856ffdb4be480cdeec781aa6a510babcdb
Jasper Bernales <bernalesjasper@gmail.com>
2023-07-23 20:48:33 +0800

    mobile responsive

--- src/views/dashboard.tsx
+++ src/views/dashboard.tsx
@@ -75,7 +75,7 @@ export default function Dashboard() {
 
   return (
     <div className='p-8 space-y-4 min-w-screen min-h-screen'>
-      <div className="flex items-center justify-between space-y-2">
+      <div className="flex sm:items-center sm:justify-between space-y-2 flex-col sm:flex-row">
         <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
         <div className="flex items-center space-x-2">
           <DateRangePicker date={date} onChange={setDate} />
